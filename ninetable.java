de5ed372e515d089ac0006f3ca0582e3faa07bf5
RoyLi511 <rrr111999o5@gmail.com>
2024-03-24 13:50:37 +0800

    打印出九九乘法表

---
+++ ninetable.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class ninetable {
+    public static void ninetable() {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " * " + j + " = " + i*j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ninetable();
+    }
+
+}
