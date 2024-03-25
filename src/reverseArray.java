0945a4ecc4096c161bef1beee954e8779db8ec04
RoyLi511 <rrr111999o5@gmail.com>
2024-03-25 22:52:49 +0800

    反轉array

--- src/reverseArray.java
+++ src/reverseArray.java
@@ -1,16 +1,22 @@
-//public class reverseArray {
-//    public static void main(String[] args) {
-//
-//        public static void reverseArray(int[] array) {
-//            for (int i = 0 ; i <=  ; i--) {
-//
-//            }
-//        }
-//
-//        int[] array = {4, 3, 6, 7, 9, 10, 33, 0, -4, 12, 400, 133, 324, 1234231, 12334};
-//        int[] reversedArray = reverseArray(array);
-//        for (int i : reversedArray) {
-//            System.out.println(i);
-//        }
-//    }
-//}
+package src;
+public class reverseArray {
+
+    public static int[] reverseArray(int[] array) {
+        int[] newArray = new int[array.length];
+        int j = 0;
+        for (int i = (array.length)-1 ; i >= 0  ; i--) {
+            newArray[j] = array[i];
+            j++;
+        }
+        return newArray;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {4, 3, 6, 7, 9, 10, 33, 0, -4, 12, 400, 133, 324, 1234231, 12334};
+        int[] reversedArray = reverseArray(array);
+        for (int i : reversedArray) {
+            System.out.print(i + " ");
+        }
+    }
+}
